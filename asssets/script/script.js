f1277b07756a412e464d2671c6af22c4cd39e795
rajkumar1206 <57241375+rajkumar1206@users.noreply.github.com>
2020-02-04 21:26:25 +0530

    I am feeling great about myself
    look out now

---
+++ asssets/script/script.js
@@ -0,0 +1,61 @@
+// PARALLAX EFFECT CODE SPINNET
+const parallax = document.querySelector('.image');
+
+parallax.style.backgroundPositionY = '0px';
+
+window.addEventListener('scroll',() => {
+    let offset = window.pageYOffset;
+    //console.log(parallax.style.backgroundPositionY);
+    parallax.style.backgroundPositionY = -offset*0.8 +'px';
+    //console.log(parallax.style.backgroundPositionY);
+});
+
+
+// TOGGLE ELEMENT CODE 
+const rotate = document.getElementsByClassName('rotate');
+const toggleElemenetList = Array.from( document.getElementsByClassName('innerblock') );
+//console.log(toggleElemenetList);
+
+const toggleHandler = (element) => {
+    element.target.nextElementSibling.classList.toggle('magic');
+}
+
+toggleElemenetList.forEach( element => {
+    element.addEventListener('click', toggleHandler.bind(this));
+});
+
+
+//PRESET OF ANIMATION CODE
+const blockList = Array.from(document.querySelectorAll('.innerblock'));
+console.log(blockList);
+
+for( let i = 0 ; i< blockList.length ;i++){
+    if(i%2==0){
+        blockList[i].style.left = "-100%";
+    } else {
+        blockList[i].style.left = "100%";
+    }
+}
+
+
+//ANIMATION EFFECT
+const id = document.getElementById('id');
+console.log(id);
+const animationHandler = document.querySelectorAll('.innerblock')[3];
+console.log(animationHandler.offsetTop);
+console.log(window.pageYOffset);
+
+window.addEventListener('scroll',() => {
+    if(id.offsetTop <= window.pageYOffset){
+        //console.log(window.pageYOffset)
+        for( let i = 0 ; i< blockList.length ;i++){
+                blockList[i].classList.add('animation');
+                setTimeout(()=>{
+                    blockList[i].style.left = '0%'
+                },1900)
+                
+        }
+    }
+    
+});
+
